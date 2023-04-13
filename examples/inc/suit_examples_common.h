c66b3a63cbf4f634a528acd47b4569c89485f8d1
Ken Takayama <ken.takayama.ietf@gmail.com>
2023-04-13 08:12:50 +0000

    update: use t_cose deterministic ecdsa repository

--- examples/inc/suit_examples_common.h
+++ examples/inc/suit_examples_common.h
@@ -8,9 +8,11 @@
 #ifndef SUIT_EXAMPLES_COMMON_H
 #define SUIT_EXAMPLES_COMMON_H
 
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include "csuit/suit_common.h"
 
 size_t read_from_file(const char *file_path, uint8_t *buf, const size_t buf_len);
 size_t write_to_file(const char *file_path, const void *buf, const size_t buf_len);
