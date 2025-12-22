4eab81cffb444df391a4fb3e18bbbaf259850311
Ken Takayama <ken.takayama.ietf@gmail.com>
2025-12-22 00:49:59 +0000

    update: min example

---
+++ examples/common.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (c) 2020-2023 SECOM CO., LTD. All Rights reserved.
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+MBEDTLS_DIR ?= ../../3rdparty/mbedtls
+QCBORE_DIR ?= ../../3rdparty/QCBOR
+T_COSE_DIR ?= ../../3rdparty/t_cose
+
+%.o: ../../src/%.c
+	$(CC) $(CFLAGS) $(INC) $(CMD_LINE) -o $@ -c $<
+
+%.o: ../%.c
+	$(CC) $(CFLAGS) $(INC) $(CMD_LINE) -o $@ -c $<
+
+%.o: %.c
+	$(CC) $(CFLAGS) $(INC) $(CMD_LINE) -o $@ -c $<
